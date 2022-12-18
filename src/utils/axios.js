5dd6d66c68be00c3bec2b806c61dd2a39d519347
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-12-18 20:51:18 +0200

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://ancient-depths-75826.herokuapp.com/api',
+    baseURL: 'https://blog-redax-server.herokuapp.com/api',
 })
 instance.interceptors.request.use(config => {
     config.headers.Authorization = window.localStorage.getItem('token')
